240cbabe70ba216993134347cb2791d73b01e91c
tanianasrin1 <tasrineva169@gmail.com>
2022-05-22 08:06:48 +0600

    navbar fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './Pages/Blogs/Blogs';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import NotFound from './Pages/NotFound/NotFound';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div>
-       <button class="btn btn-warning">Warning</button>
-       <button class="btn btn-warning">Warning</button>
+       <Navbar></Navbar>
+
+       <Routes>
+         <Route path="/" element={<Home></Home>}></Route>
+         <Route path="blogs" element={<Blogs></Blogs>}></Route>
+         <Route path="blogs" element={<Blogs></Blogs>}></Route>
+         <Route path="login" element={<Login></Login>}></Route>
+         <Route path="/*" element={<NotFound></NotFound>}></Route>
+       </Routes>
+       
     </div>
   );
 }
