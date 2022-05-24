38a55ef56ca127aea84644a526f12034df6e64e2
tanianasrin1 <tasrineva169@gmail.com>
2022-05-24 14:20:28 +0600

    purchase  page fixed

--- src/Pages/Home/Tool.js
+++ src/Pages/Home/Tool.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Tool = ({tool}) => {
-    const {name, img, price, description, quantity} = tool;
+    const {name, img, price, description, quantity, min} = tool;
     return (        
         <div class="card w-96 bg-base-100 shadow-xl">
   <figure class="px-10 pt-10">
@@ -11,10 +11,12 @@ const Tool = ({tool}) => {
   <div class="card-body items-center text-center">
   
       <h4>Name: {name} </h4>
-      <h6>name: {price} </h6>
+      <h6> Price : {price} </h6>
       <p>Des: {description} </p>
       <p>Price: {price} </p>
-      <p>Available-quantity: {quantity} </p>
+      <p>Available-Quantity: {quantity} </p>
+      <p>Min-Order-Quantity: {min} </p>
+      
     
     <div class="card-actions">
       <Link className="btn btn-primary" to = '/purchase'>Buy Now</Link>
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import {getAuth} from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA2zDcw-kAn18qcdSeFb16zVmcjnvwCTEU",
-  authDomain: "leptop-tools-bd.firebaseapp.com",
-  projectId: "leptop-tools-bd",
-  storageBucket: "leptop-tools-bd.appspot.com",
-  messagingSenderId: "886327786074",
-  appId: "1:886327786074:web:74af6ebe8be2a3925fb663"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
