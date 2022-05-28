40ceab5d0af6a36f9e46974d23cd7f1e46c894e8
tanianasrin1 <tasrineva169@gmail.com>
2022-05-28 15:12:03 +0600

    product page fixed

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import bg from '../../images/image/f (1).jpg'
+import './NotFound.css'
 
 const NotFound = () => {
     return (
         <div>
-            <h2>This is not found page</h2>
+            <h2 className='text-3xl my-6 text-center text-purple-500'>Not Found!!!</h2>
+            <img className='img-design mb-6' src={bg} alt="" />
         </div>
     );
 };
